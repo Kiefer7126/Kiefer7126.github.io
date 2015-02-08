2572d8b86d436b5b7b393f2d42df3a6cb73aa118
Sawada Shun <b1012046@fun.ac.jp>
2015-02-08 17:56:52 +0900

    Exchange Calculator has been Completed

--- exchange.js
+++ exchange.js
@@ -1,60 +1,60 @@
-var selectedCurrency = "dollar";
+var selectedCurrency = " dollars";
 
 function changeCurrency(){
-	var currency = document.getElementById("currency");
-	selectedCurrency = currency.options[currency.selectedIndex].value;
+  var currency = document.getElementById("currency");
+  selectedCurrency = currency.options[currency.selectedIndex].value;
 
-	var rateElement = document.getElementById("rate");
+  var rateElement = document.getElementById("rate");
+  
+  switch(selectedCurrency){
+    case "Dollars":
+      rateElement.value = 77.8;
+      break;
+    case "Euros":
+      rateElement.value = 102.3;
+      break;
+    case "Yuans":
+      rateElement.value = 12.3;
+      break;
+    default:
+      rateElement.value = 1;
+      break;
+  }
 
-	switch(selectedCurrency){
-		case "dollar":
-			rateElement.value = 77.8;
-			break;
-		case "euro":
-			rateElement.value = 102.3;
-			break;
-		case "yuan":
-			rateElement.value = 12.3;
-			break;
-		default:
-			rateElement.value = 1;
-			break;
-	}
-
-	document.getElementById("buttonExchange").value = "exchange " + selectedCurrency + " for yens.";
-	document.getElementById("buttonExchange2").value = "exchange yens for " + selectedCurrency + ".";
+  document.getElementById("buttonExchange").value = selectedCurrency + " -> Yens";
+  document.getElementById("buttonExchange2").value = "Yens -> " + selectedCurrency;
 }
 
 function exchange(exchangeType){
-	var rate = document.getElementById("rate").value;
-	var inMoney = document.getElementById("inMoney").value;
+  var rate = document.getElementById("rate").value;
+  var inMoney = document.getElementById("inMoney").value;
 
-	var exchangeMoney;
-	var currency;
+  var exchangeMoney;
+  var currency;
 
-	if(exchangeType == "toYen"){
-		exchangeMoney = inMoney * rate;
-		currency = "yen";
-	}else{
-		exchangeMoney = inMoney / rate;
-		//currency = " dollars"
-		currency = selectedCurrency;
-	}
+  if(exchangeType == "toYen"){
+    exchangeMoney = inMoney * rate;
+    currency = " Yens";
+  }else{
+    exchangeMoney = inMoney / rate;
+    //currency = " dollars"
+    currency = selectedCurrency;
+  }
 
 //小数点第三位で四捨五入
 //100倍してから整数に丸めて100で割る
-	exchangeMoney = Math.round(exchangeMoney * 100) / 100; 
+  exchangeMoney = Math.round(exchangeMoney * 100) / 100; 
 
-	if(isNaN(exchangeMoney)){
-		alert("数値を入力してください。");
-		exchangeMoney = "---";
-	}
+  if(isNaN(exchangeMoney)){
+    alert("数値を入力してください。");
+    exchangeMoney = "---";
+  }
 
-	if(rate <= 0){
-		alert("為替レートは0より大きな数値を入力してください。");
-		exchangeMoney = "---";
-	}
+  if(rate <= 0){
+    alert("為替レートは0より大きな数値を入力してください。");
+    exchangeMoney = "---";
+  }
 
-	var outMoneyElement = document.getElementById("outMoney");
-	outMoneyElement.innerHTML = exchangeMoney + currency;
+  var outMoneyElement = document.getElementById("outMoney");
+  outMoneyElement.innerHTML = exchangeMoney + " " + currency;
 }
