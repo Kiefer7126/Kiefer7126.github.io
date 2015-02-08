54107a075a3d92c5a684e2aa975a9ac73ac78ee9
Sawada Shun <b1012046@fun.ac.jp>
2015-02-08 17:05:56 +0900

    Add dollar, euro and yuan

--- exchange.js
+++ exchange.js
@@ -1,3 +1,30 @@
+var selectedCurrency = "dollar";
+
+function changeCurrency(){
+	var currency = document.getElementById("currency");
+	selectedCurrency = currency.options[currency.selectedIndex].value;
+
+	var rateElement = document.getElementById("rate");
+
+	switch(selectedCurrency){
+		case "dollar":
+			rateElement.value = 77.8;
+			break;
+		case "euro":
+			rateElement.value = 102.3;
+			break;
+		case "yuan":
+			rateElement.value = 12.3;
+			break;
+		default:
+			rateElement.value = 1;
+			break;
+	}
+
+	document.getElementById("buttonExchange").value = "exchange " + selectedCurrency + " for yens.";
+	document.getElementById("buttonExchange2").value = "exchange yens for " + selectedCurrency + ".";
+}
+
 function exchange(exchangeType){
 	var rate = document.getElementById("rate").value;
 	var inMoney = document.getElementById("inMoney").value;
@@ -7,10 +34,11 @@ function exchange(exchangeType){
 
 	if(exchangeType == "toYen"){
 		exchangeMoney = inMoney * rate;
-		currency = " yens";
+		currency = "yen";
 	}else{
 		exchangeMoney = inMoney / rate;
-		currency = " dollars"
+		//currency = " dollars"
+		currency = selectedCurrency;
 	}
 
 //小数点第三位で四捨五入
