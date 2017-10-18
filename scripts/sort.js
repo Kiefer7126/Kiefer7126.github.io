c30d3825f66c795e634c42c8608147d192c45b94
kiefer7126 <b1012046@gmail.com>
2017-10-19 00:58:45 +0900

    Add transposed matrix

--- scripts/sort.js
+++ scripts/sort.js
@@ -24,3 +24,53 @@ function inverse() {
     document.getElementById('textareaAfter').value = resultString;
     $('#textareaAfter').trigger('autoresize');
 }
+
+function transpose() {
+    var text = document.getElementById('textareaTransBefore').value;
+    var lineArray = text.split(/\r\n|\r|\n/);
+    var elementArray = [];
+
+    for (var i = 0; i < lineArray.length; i++) {
+        elementArray[elementArray.length] = lineArray[i].split(/\|/);
+        elementArray[elementArray.length - 1].shift();
+        elementArray[elementArray.length - 1].pop();
+    }
+
+    var transElementArray = [];
+    var transLineArray = [];
+    var temptransElementArray = [];
+
+    for (var j = 0; j < elementArray[0].length; j++) {
+        temptransElementArray.length = 0;
+
+        for (var i = 0; i < elementArray.length; i++) {
+            temptransElementArray[temptransElementArray.length] = elementArray[i][j];
+        }
+        console.log("temptransElementArray");
+        console.log(temptransElementArray);
+        transElementArray = temptransElementArray.concat();
+        transLineArray[transLineArray.length] = transElementArray;
+    }
+    console.log("lineArray");
+    console.log(lineArray);
+    console.log("elementArray");
+    console.log(elementArray);
+    console.log("transElementArray");
+    console.log(transElementArray);
+    console.log("transLineArray");
+    console.log(transLineArray);
+    
+    
+    var resultString = '';
+    for (var i = 0; i < transLineArray.length; i++) {
+        resultString += "\|"
+        for(var j = 0; j < transLineArray[0].length; j++){
+        resultString += transLineArray[i][j] + "\|";
+        }
+        resultString += "\n"
+    }
+    
+        document.getElementById('textareaTransAfter').value = resultString;
+    $('#textareaTransAfter').trigger('autoresize');
+
+}
